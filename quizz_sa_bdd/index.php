1a473e5d94180da17fcf3667fcfd5cffca9ac7cd
abiteye <62648130+abiteye@users.noreply.github.com>
2020-06-15 04:24:31 +0000

    Add files via upload

--- quizz_sa_bdd/index.php
+++ quizz_sa_bdd/index.php
@@ -1,71 +1,65 @@
 <?php
 session_start();
-        require_once './traitements/fonctions.php';
-
-        if(isset($_GET['action']))
-    {
-
-        if($_GET['action']=='connexion')
-        {
-            $data=$_POST;
-
-            pageConnexion($data);
-        }
-        else if($_GET['action']=='inscription')
-        {
-            require_once './pages/inscription.php';
-        }
-        else if($_GET['action'] == 'admin')
-        {
-            if($_GET['page'] == 'liste')
-            {
-                require_once './pages/liste_question.php';
-            }
-            elseif($_GET['page'] == 'inscription')
-            {
-                require_once './pages/inscription.php';
-
-            }
-            elseif($_GET['page'] == 'joueur')
-            {
-                require_once './pages/joueur.php';
-
-            }
-            elseif($_GET['page'] == 'question')
-            {
-                require_once './pages/questions.php';
-
-            }
-            else
-            {
-                require_once './pages/liste_question.php';
-            }
-        }
-    }
-    else
-    {
-            require_once './pages/template.php';
-    }
-
-        /*session_start();
-        require_once('./traitements/fonctions.php');
-        require_once './data/requete.php';
-
-        if(isset($_GET['lien'])) {
-            switch($_GET['lien']) { 
-                case "accueil":
-                    require_once('./pages/accueil.php'); 
-                break;    
-                case "jeux":
-                    require_once('./pages/jeux.php');
-                break;    
-                   
-            }
-        }else{
-            if(isset($_GET['statut']) && $_GET['statut']==="logout"){ 
-                deconnexion();
-            }
-            require_once('./pages/template.php');
-        }*/
-?>
         
+    define("WEBROOT","http://localhost/Projets/quizz_sa_bd");
+    define("ACTION","action");
+    require_once("./traitements/fonctions.php");
+    require_once("./data/requete.php");
+    require_once('./data/getJoueur.php');
+       
+        if(isset($_GET['action'])) {
+         
+            if($_GET['action']=='connexion'){ 
+           
+               $data=$_POST;
+
+               pageConnexion($data);
+            }
+            else if($_GET['action']=='inscription'){ 
+           
+               require_once './pages/inscription.php';
+            }
+            else if($_GET['action'] == 'admin'){ 
+           
+                if(is_connect()){
+                    
+                    if($_GET['page'] == 'liste')
+                    {
+                        require_once './pages/liste_question.php';
+                    }
+                    elseif($_GET['page'] == 'inscription')
+                    {
+                        require_once './pages/inscription.php';
+                    }
+                    elseif($_GET['page'] == 'joueur')
+                    {
+                        require_once './pages/joueur.php';
+                    }
+                    elseif($_GET['page'] == 'question')
+                    {
+                        require_once './pages/questions.php';
+                    }
+                    else
+                    {
+                        require_once './pages/liste_question.php';
+                    }
+                }elseif($_GET[ACTION]=="joueur"){
+
+                    if(is_connect()){
+                        require_once './pages/jeux.php';
+                    }
+                  
+                }
+                elseif($_GET[ACTION]=="deconnexion"){
+                      //Traitement de Deconnexion
+                       deconnection();
+                }
+            }
+        }
+        else
+        {
+            require_once './pages/template.php';
+        }
+   
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
